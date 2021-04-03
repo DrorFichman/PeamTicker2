dbf9077cc222a214736cdd9d5a794776503a44d5
Dror Fichman <drorfichman@gmail.com>
2021-04-03 16:45:46 +0300

    account data last usage

--- app/src/main/java/com/teampicker/drorfichman/teampicker/Data/AccountData.java
+++ app/src/main/java/com/teampicker/drorfichman/teampicker/Data/AccountData.java
@@ -1,17 +1,16 @@
 package com.teampicker.drorfichman.teampicker.Data;
 
-import android.net.Uri;
-
 import com.google.firebase.auth.FirebaseUser;
 
 public class AccountData {
 
-    public String email = "asa";
-    public String displayName = "aaa";
+    public String email;
+    public String displayName;
     public String uid;
     public String photoUrl;
-    public long lastSignInTimestamp;
-    public long creationTimestamp;
+    public long lastSignIn;
+    public long creation;
+    public long lastUsage;
 
     public AccountData(FirebaseUser user) {
         if (user != null) {
@@ -20,9 +19,10 @@ public class AccountData {
             this.uid = user.getUid();
             this.photoUrl = user.getPhotoUrl().toString();
             if (user.getMetadata() != null) {
-                this.lastSignInTimestamp = user.getMetadata().getLastSignInTimestamp();
-                this.creationTimestamp = user.getMetadata().getCreationTimestamp();
+                this.lastSignIn = user.getMetadata().getLastSignInTimestamp();
+                this.creation = user.getMetadata().getCreationTimestamp();
             }
+            this.lastUsage = System.currentTimeMillis();
         }
     }
 }
